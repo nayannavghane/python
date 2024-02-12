7bc78b98574b84abb693384a887fe004add5de2d
garvitsinghal <garvitsinghal@200.com>
2024-02-12 13:43:34 +0530

    added a new project

--- evenodd.py
+++ evenodd.py
@@ -1,5 +1,8 @@
 num = int(input("enter a number"))
 if (num % 2) == 0:
     print("The number is even")
+elif:
+    ("The number you entered are special charcter")
 else:
     print("The provide the number is  odd")
+    
